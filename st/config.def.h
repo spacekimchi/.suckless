81aee2ec8434631bb8ee3d84ac9c60313ea88abb
spacekimchis <spacekimchis@gmail.com>
2023-04-26 08:39:20 -0700

    apply scrolling patch to st

--- st/config.def.h
+++ st/config.def.h
@@ -5,8 +5,11 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "FiraCode Nerd Font:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
+
+/* bg opacity (Requires alpha patch) */
+float alpha = 0.7;
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -92,9 +95,6 @@ char *termname = "st-256color";
  *	stty tabs
  */
 unsigned int tabspaces = 8;
-
-/* bg opacity */
-float alpha = 0.7;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
@@ -204,6 +204,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
 
 /*
--- st/config.h
+++ st/config.h
@@ -11,7 +11,6 @@ static int borderpx = 2;
 /* bg opacity */
 float alpha = 0.7;
 
-
 /*
  * What program is execed by st depends of these precedence rules:
  * 1: program passed with -e
@@ -205,6 +204,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
 
 /*
